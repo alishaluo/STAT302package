d489970510ef9fae9ce700d670a94e97ad5d8e44
Alisha Luo <luo.alisha@gmail.com>
2020-03-09 17:25:51 -0700

    check t-test

--- R/my_t_test.R
+++ R/my_t_test.R
@@ -18,30 +18,35 @@ my_t.test <- function(x, alternative, mu) {
   # gives the alternative hypothesis statements
   if(alternative == "two.sided") {
     cat("true difference in means is not equal to", mu)
+    print(" ")
   }else if (alternative == "less") {
     cat("true difference in means is less than", mu)
+    print(" ")
   }else if (alternative == "greater") {
     cat("true difference in means is greater than", mu)
+    print(" ")
   } else {
     print("Error: invalid argument")
   }
   # calculates standard error
   se.t_test <- sd(x)/(sqrt(length(x)))
+  # calculates test statistic
+  test_stat <- ((mean(x) - mu)/se.t_test)
+  # calculates degrees of freedom
+  df <- (length(x)) - 1
+  # if it is a two sided t-test, it will calculate the p value accordingly
+  if(alternative == "two.sided") {
+    p_val <- 2 * pt(abs(test_stat), df = df, lower.tail = FALSE)
+  } else {
+    # calculates for p value for a one-sided t-test
+    p_val <- pt(abs(test_stat), df = df, lower.tail = FALSE)
+  }
   # creates a list
   my_result <- list(
-    # calculates test statistic
-    test_stat <- ((mean(x) - mu)/se.t_test),
-    # calculates degrees of freedom
-    df <- (length(x)) - 1,
-    # gives alternative hypothesis
+    "test statistic" = test_stat,
+    "df" = df,
     "alternative" = alternative,
-    # if it is a two sided t-test, it will calculate the p value accordingly
-    if(alternative == "two.sided") {
-      p_val <- 2 * pt(abs(test_stat), df = df, lower.tail = FALSE)
-    } else {
-      # calculates for p value for a one-sided t-test
-      p_val <- pt(abs(test_stat), df = df, lower.tail = FALSE)
-    }
+    "p_value" = p_val
   )
   # returns the resulting list
   return(my_result)
