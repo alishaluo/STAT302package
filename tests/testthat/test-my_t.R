8d02d109c97a3d9f646e95c9f5e5d57a380c951d
Alisha Luo <luo.alisha@gmail.com>
2020-03-11 01:21:55 -0700

    edit t-test test

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,4 +1,4 @@
-# within my_t.test.R
+# within test-my_t.test.R
 test_that("t-test works", {
   expect_is(my_t.test(rnorm(10, 0, 1), alternative = "less", mu = 0), "list")
   expect_is(my_t.test(rnorm(10, 0, 1), alternative = "less", mu = 0)$p_value,
