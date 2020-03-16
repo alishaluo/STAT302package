85601ff2379fcf238fc7f526a1ee4ed5006e0629
Alisha Luo <luo.alisha@gmail.com>
2020-03-16 14:38:56 -0700

    add tests

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -1,6 +1,8 @@
 # within test-my_t_test.R
 test_that("t-test works", {
   expect_is(my_t_test(rnorm(10, 0, 1), alternative = "less", mu = 0), "list")
+  expect_is(my_t_test(rnorm(10, 0, 1), alternative = "two.sided", mu = 0), "list")
+  expect_is(my_t_test(rnorm(10, 0, 1), alternative = "greater", mu = 0), "list")
   expect_is(my_t_test(rnorm(10, 0, 1), alternative = "less", mu = 0)$p_value,
             "numeric")
 })
